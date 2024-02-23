5b84245bcd09a57226f00f77c6bfbc05e276c913
Rob Tillaart <rob.tillaart@gmail.com>
2024-02-23 12:57:49 +0100

    initial version

---
+++ examples/74HC138_demo/74HC138_demo.ino
@@ -0,0 +1,36 @@
+//
+//    FILE: 74HC138_demo.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: test basic behaviour and performance
+//     URL: https://github.com/RobTillaart/
+
+
+#include "74HC138.h"
+
+
+DEV_74HC138 selector(5,6,7);
+
+uint8_t line = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(__FILE__);
+  Serial.print("LIB_74HC138_VERSION: ");
+  Serial.println(LIB_74HC138_VERSION);
+}
+
+
+void loop()
+{
+  Serial.println(selector.getLine());
+  delay(100);
+  selector.setLine(line);
+  line++;
+  if (line >= 8) line = 0;
+  delay(1000);
+}
+
+
+//  -- END OF FILE --
